8abb45f50949e867ff7312b2b5b2b1fdb8a05c50
ljc-dev <dev.if.ljc@gmail.com>
2020-10-08 10:04:40 +0200

    stopwatch logic update

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*
+
 function call(name) {
   return document.querySelector(name);
 }
@@ -15,16 +17,14 @@ let secondsCounter = 1,
 
 startBtn.addEventListener("click", () => {
   let minute = setInterval(() => {
-    display1.textContent = `${
-      minuteCounter < 10 ? (a = 0) : (a = "")
-    }${minuteCounter++}`;
+    display1.textContent = `${minuteCounter < 10 ? (a = 0) : (a = "")
+      }${minuteCounter++}`;
   }, 60000);
 
   let seconds = setInterval(() => {
     if (secondsCounter < 60) {
-      display2.textContent = `${
-        secondsCounter < 10 ? (a = 0) : (a = "")
-      }${secondsCounter}`;
+      display2.textContent = `${secondsCounter < 10 ? (a = 0) : (a = "")
+        }${secondsCounter}`;
       secondsCounter++;
     } else {
       secondsCounter = 1;
@@ -39,6 +39,54 @@ startBtn.addEventListener("click", () => {
   });
 
   resetBtn.addEventListener("click", () => {
-    bjjjds;
+    secondsCounter = 1
+    minuteCounter = 1
+    clearInterval(minute);
+    // console.log(secondsCounter);
+    clearInterval(seconds);
+    // console.log(seconds);
+    display1.textContent = "00"
+    display2.textContent = "00"
   });
 });
+
+*/
+
+const startBtn = document.querySelector("#start")
+const pauseBtn = document.querySelector("#pause")
+const resetBtn = document.querySelector("#reset")
+const displayMins = document.querySelector("#display1")
+const displaySecs = document.querySelector("#display2")
+let interval, totalSecs = 0
+
+function startWatch() {
+  interval = setInterval(() => {
+    totalSecs++
+    updateDisplay()
+  }, 1000);
+}
+
+function pauseWatch() {
+  if (interval) clearInterval(interval)
+}
+
+function resetWatch() {
+  if (interval) clearInterval(interval)
+  totalSecs = 0
+  updateDisplay()
+}
+
+function addZeros(number) {
+  return number < 10 ? "0" + number : number
+}
+
+function updateDisplay() {
+  const mins = Math.trunc(totalSecs / 60)
+  const secs = totalSecs % 60
+  displayMins.innerText = addZeros(mins)
+  displaySecs.innerText = addZeros(secs)
+}
+
+startBtn.addEventListener("click", startWatch)
+pauseBtn.addEventListener("click", pauseWatch)
+resetBtn.addEventListener("click", resetWatch)
